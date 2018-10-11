26673dec8aebf774e161c84c34dde60bafe24b06
Adam Goldfine <tulaneadam21@gmail.com>
2018-10-10 23:27:41 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -2,27 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    // change code below this line
+this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    this.reset = this.reset.bind(this)
+    // change code above this line
+  }
+  // change code below this line
+increment() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  reset() {
+    this.setState({
+      count: 0
+    })
+  }
+  // change code above this line
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
       </div>
     );
   }
-}
+};
 
 export default App;
